7bb317c1eea110ec3f6342ea7aefad7f7d92eb4f
sgt <chen09869@gmail.com>
2019-06-20 16:20:56 +0800

    完成贝塞尔波浪线效果

--- lib/customCliper.dart
+++ lib/customCliper.dart
@@ -11,7 +11,7 @@ class CustomCliper extends StatelessWidget {
       body:Column(
         children: <Widget>[
           ClipPath( 
-            clipper:BottomClipper(),  // 控制裁剪路径
+            clipper:BottomClipper2(),  // 控制裁剪路径
             child: Container(   // 所要裁剪的子元素
               color:Colors.blueAccent,
               height: 200.0,
@@ -44,6 +44,36 @@ class BottomClipper extends CustomClipper<Path>{
   bool shouldReclip(CustomClipper<Path> oldClipper) {
     return false;
   }
+  
+}
+
+
+// 波浪形曲线
+class BottomClipper2 extends CustomClipper<Path>{
+  @override
+    Path getClip(Size size) {
+      var path = Path();
+      path.lineTo(0, size.height-20);
+      var firstControlPoint =Offset(size.width/4,size.height);
+      var firstEndPoint = Offset(size.width/2.25,size.height-30);
+
+      path.quadraticBezierTo(firstControlPoint.dx, firstControlPoint.dy, firstEndPoint.dx, firstEndPoint.dy);
+
+      var secondControlPoint = Offset(size.width/4*3,size.height-80);
+      var secondEndPoint = Offset(size.width,size.height-40);
+
+      path.quadraticBezierTo(secondControlPoint.dx, secondControlPoint.dy, secondEndPoint.dx, secondEndPoint.dy);
+
+      path.lineTo(size.width, size.height-40);
+      path.lineTo(size.width, 0);
+
+      return path;
+
+    }
+    @override
+    bool shouldReclip(CustomClipper<Path> oldClipper) {
+      return false;
+    }
   
 }
 
