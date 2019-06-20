3a4a24bdfd45e365e76b8081263e29fd0b91b04a
sgt <chen09869@gmail.com>
2019-06-20 16:48:09 +0800

    增加曲线

--- lib/customCliper.dart
+++ lib/customCliper.dart
@@ -11,7 +11,7 @@ class CustomCliper extends StatelessWidget {
       body:Column(
         children: <Widget>[
           ClipPath( 
-            clipper:BottomClipper2(),  // 控制裁剪路径
+            clipper:BottomClipper3(),  // 控制裁剪路径
             child: Container(   // 所要裁剪的子元素
               color:Colors.blueAccent,
               height: 200.0,
@@ -116,4 +116,34 @@ class _StarCliper extends CustomClipper<Path>{
   bool shouldReclip(_StarCliper oldClipper) {
     return this.radius != oldClipper.radius;
   }
+}
+
+// 曲线3
+class BottomClipper3 extends CustomClipper<Path>{
+  @override
+    Path getClip(Size size) {
+      var path = Path();
+      path.lineTo(0, 0);
+      path.lineTo(0, size.height-100);
+      var firstControlPoint =Offset(size.width/4*1,size.height-150);
+      var firstEndPoint = Offset(size.width/4*2,size.height);
+
+      path.quadraticBezierTo(firstControlPoint.dx, firstControlPoint.dy, firstEndPoint.dx, firstEndPoint.dy);
+
+      var secondControlPoint = Offset(size.width/4*3,size.height-150);
+      var secondEndPoint = Offset(size.width,size.height-100);
+
+      path.quadraticBezierTo(secondControlPoint.dx, secondControlPoint.dy, secondEndPoint.dx, secondEndPoint.dy);
+
+      path.lineTo(size.width, size.height-100);
+      path.lineTo(size.width, 0);
+
+      return path;
+
+    }
+    @override
+    bool shouldReclip(CustomClipper<Path> oldClipper) {
+      return false;
+    }
+  
 }
